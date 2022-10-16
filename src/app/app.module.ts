77687bf200de00a9281904a454804b7b174119e3
Roberta Bononi da Silva <roberta.bononi16@gmail.com>
2022-10-16 17:20:38 -0300

    Desenvolvimento layout cards informativos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MenuLateralComponent } from './components/menu-lateral/menu-lateral.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuLateralComponent
+    MenuLateralComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule
