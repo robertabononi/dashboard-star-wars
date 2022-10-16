f634e608133253b7ffd82cd82f42312095684566
Roberta Bononi da Silva <roberta.bononi16@gmail.com>
2022-10-16 15:16:31 -0300

    Remoção de pasta duplicada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MenuLateralComponent } from './components/menu-lateral/menu-lateral/menu-lateral.component';
+import { MenuLateralComponent } from './components/menu-lateral/menu-lateral.component';
 
 @NgModule({
   declarations: [
